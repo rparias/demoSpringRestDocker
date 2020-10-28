b97f74bce475d201c5b89846f2db8d473acda866
Ronald Arias <ronald.arias@thoughtworks.com>
2020-10-28 12:49:10 -0500

    Adds project

---
+++ demo-apirest-crud/src/main/java/com/ronaldarias/apirest/controllers/CustomerRestController.java
@@ -0,0 +1,79 @@
+package com.ronaldarias.apirest.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ronaldarias.apirest.models.entity.Customer;
+import com.ronaldarias.apirest.models.service.CustomerService;
+
+@CrossOrigin(origins = {"http://localhost:4200"})
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+
+    // injection dependency
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/customers")
+    public List<Customer> getCustomers() {
+        return customerService.getCustomers();
+    }
+
+    @GetMapping("/customers/{customerId}")
+    public Customer getCustomer(@PathVariable int customerId) {
+
+        Customer customer = customerService.getCustomer(customerId);
+
+        return customer;
+    }
+
+    // add mapping for POST /customers - add new customer
+
+    @PostMapping("/customers")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer addCustomer(@RequestBody Customer customer) {
+
+        // also just in case the pass an id in JSON ... set id to 0
+        // this is force a save of new item ... instead of update
+
+        customer.setId(null);
+
+        customerService.saveCustomer(customer);
+
+        return customer;
+    }
+
+    // add mapping for PUT /customers - update existing customer
+
+    @PutMapping("/customers")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer updateCustomer(@RequestBody Customer customer) {
+
+        customerService.saveCustomer(customer);
+
+        return customer;
+    }
+
+
+    //add mapping for DELETE /customers/{customerId} - delete existing customer
+    @DeleteMapping("/customers/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable int customerId) {
+
+        customerService.deleteCustomer(customerId);
+    }
+
+}
